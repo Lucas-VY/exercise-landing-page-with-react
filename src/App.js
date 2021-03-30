52faf6259ea0b2ad0d57b855fb40ed8bf904f342
LucasVY <lucasvy@LucasVYs-MacBook-Pro.local>
2021-03-30 17:29:24 -0300

    jumbotron

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import Jumbotron from './components/Jumbotron'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    < Jumbotron title ="A Warm Welcome!"/> 
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+        </div>
+      </div>
     </div>
+ </>
+
   );
 }
 
 export default App;
+
+
