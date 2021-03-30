47079aa5aac5cc37dc8c9024d70eff50d39010ce
LucasVY <lucasvy@LucasVYs-MacBook-Pro.local>
2021-03-30 18:31:10 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -3,25 +3,6 @@ import Jumbotron from './components/Jumbotron'
 import { Card } from './components/Card';
 import { cardTitles } from "./components/data";
 
-<<<<<<< HEAD
-import './App.css';
-import {Card} from './components/card';
-import {cardTitles} from "./components/data";
-
-
-
-
-const App = props => {
-  return(
-  <>
-  <div className="row p-1">
-  { cardTitles.map((title, i) => {
-    return <Card key={i} text= {title}/>
-  })}
-  </div>
-   </>
-  )
-=======
 function App() {
   return (
     <>
@@ -40,6 +21,5 @@ function App() {
     </>
 
   );
->>>>>>> 092fd828f2ed7eff9890251a46192c45b2d84763
 }
 export default App;
