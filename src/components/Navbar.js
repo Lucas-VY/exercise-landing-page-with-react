d98df113e80016aa758ecc22da66738b128c9dc2
Alex <axherrerax@gmail.com>
2021-03-31 11:59:32 -0300

    Se soluciono boton responsive y margin al navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,31 +4,30 @@ const Navbar=(infor) =>{
     return(
 
 
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
-        <div className="container-fluid">
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4 ">
+        <div className="container-fluid ">
             <a className="navbar-brand" href="#">{infor.inicio}</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
 
-                <ul className="navbar-nav">
-                    
-                    <li className="nav-item">
-                        <a class="nav-link active"  href="#">{infor.home}</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" href="#">{infor.about}</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" href="#">{infor.services}</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" href="#">{infor.contact}</a>
-                    </li>
-
-                </ul>
+            <ul class="navbar-nav ">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">{infor.home}<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">{infor.about}</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">{infor.services}</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">{infor.contact}</a>
+      </li>
+      
+    </ul>
             </div>
         </div>
     </nav>
