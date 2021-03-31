8804706994d51068944dcf254fc536e3277ea202
Sarai Santiago <saraisantiagom@gmail.com>
2021-03-31 11:04:21 -0300

    aumentando margen inferior de cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 export const Card = ({text}) => {
     return (
 		<div className="col-xs-12 col-sm-6 col-md-6 col-lg-3">
-        <div className="card mb-3">
+        <div className="card mb-3k">
 		<img alt="Card image cap" className="card-img-top" src="https://picsum.photos/400/400?random=1" />
 		<div className="card-body">
 			<h5 className="card-title">{text}</h5>
